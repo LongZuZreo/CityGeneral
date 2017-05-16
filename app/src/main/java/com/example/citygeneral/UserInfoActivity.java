4fd070d1cf41518c98628a83d8035d2ec2ed3525
LongZuZero <LongZuZero@gmail.com>
2017-05-16 21:08:13 +0800

    完成radio的动态切换，并添加了所有空的Fragment

---
+++ app/src/main/java/com/example/citygeneral/UserInfoActivity.java
@@ -0,0 +1,39 @@
+package com.example.citygeneral;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.citygeneral.base.BaseActivity;
+
+/**
+ * Created by 张志远 on 2017/5/15.
+ */
+
+public class UserInfoActivity extends BaseActivity{
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.user_info_activity;
+    }
+
+    @Override
+    protected void initView() {
+        
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
---
+++ app/src/main/java/com/example/citygeneral/fragment/MesFragment.java
@@ -0,0 +1,52 @@
+package com.example.citygeneral.fragment;
+
+import android.view.View;
+
+import com.example.citygeneral.R;
+import com.example.citygeneral.base.BaseFragment;
+
+/**
+ * Created by 张志远 on 2017/5/16.
+ */
+
+public class MesFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_headline;
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void show() {
+
+    }
+
+    @Override
+    protected void hide() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
